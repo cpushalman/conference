eb05dee3ca9df9d321ff85e450e34884d22e670c
Teju Beast <sundarapuram6@gmail.com>
2025-09-07 22:50:38 +0530

    added few more

--- src/components/CommitteeList.jsx
+++ src/components/CommitteeList.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 
 export default function CommitteeList() {
   const [members, setMembers] = useState([]);
+  const [tech,setTech]=useState([]);
+  const [pro,setPro]=useState([]);
 
   useEffect(() => {
     fetch("/data/committee.json")
@@ -10,9 +12,22 @@ export default function CommitteeList() {
       .catch((err) => console.error("Failed to load committee data:", err));
   }, []);
 
+  useEffect(() => {
+    fetch("/data/techcommittee.json")
+      .then((res) => res.json())
+      .then((data) => setTech(data))
+      .catch((err) => console.error("Failed to load committee data:", err));
+  }, []);
+  useEffect(() => {
+    fetch("/data/procommittee.json")
+      .then((res) => res.json())
+      .then((data) => setPro(data))
+      .catch((err) => console.error("Failed to load committee data:", err));
+  }, []);
+
   return (
     <div className="container">
-      <h1>Committee Members</h1>
+      <h1>ADVISORY COMMITTEE</h1>
 
       <div className="members-list">
         {members.map((member) => (
@@ -23,7 +38,31 @@ export default function CommitteeList() {
             <p>{member.university}</p>
           </div>
         ))}
-      </div>
+      </div><br />
+      <h1>ADVISORY COMMITTEE</h1>
+      <div className="members-list">
+        {tech.map((member) => (
+          <div key={member.id} className="member-card">
+            <img src={member.image} alt={member.name} width="150" />
+            <h3>{member.name}</h3>
+            <p><strong>{member.designation}</strong></p>
+            <p>{member.university}</p>
+          </div>
+        ))}
+      </div><br />
+
+      <h1>PROGRAM COMMITTEE</h1>
+      <div className="members-list">
+        {pro.map((member) => (
+          <div key={member.id} className="member-card">
+            <img src={member.image} alt={member.name} width="150" />
+            <h3>{member.name}</h3>
+            <p><strong>{member.designation}</strong></p>
+            <p>{member.university}</p>
+          </div>
+        ))}
+      </div><br />
+
     </div>
   );
 }
